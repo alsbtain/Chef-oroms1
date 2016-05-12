1bcc1fff72927e8dcb801da02e8a9143165d8239
alsbtain <info@alsbtain.net>
2016-05-12 10:51:38 +0300

    done

---
+++ js/app.js
@@ -0,0 +1,168 @@
+(function(){
+
+
+
+    checkReply = function(data) {
+        if (typeof(data.status) != "undefined") {
+           
+            alert(data.message);
+            return false;
+        } else {
+            return true;
+        }
+    };
+    server = {
+         url:"http://sbta.in/websites/oroms/service/",
+            request: function(options) {
+                return $.post(server.url+"index.php", options).promise();
+            },
+            debug: function(options) {
+                return $.post(server.url+"debug.php", options).promise();
+            }
+
+       
+       };
+       
+function loadScript(){
+	
+	  var top = $("#scripts");
+            top.html(" <script src='./js/jQuery.2.1.js'></script> "+"<script src='./js/webflow.js'></script> "
+            +"<script>$(window).load(function(){$('.loaddiv').fadeOut(5000);});</script>");
+}  
+  function notificationcorrect(strblue){
+	
+		    var top = $("#notificationblue");
+		    var top1=$("#successCode");
+		    top1.html("<h3 class='notification-text'>"+strblue+"</h3>");
+            top.click();
+
+}
+      function notificationincorrect(strred){
+	
+		    var top = $("#notificationred");
+		    var top1=$("#errorCode");
+		    top1.html("<h3 class='notification-text'>"+strred+"</h3>");
+            top.click();
+
+}
+
+
+  
+				var isemployee  = localStorage.getItem("isemployee");
+				var login  = localStorage.getItem("login");
+				console.log(isemployee)
+
+
+$usersession = {
+
+
+	
+	
+	
+};
+
+  
+             
+
+	function loadorders(){
+        	
+  
+            server.request({route:{app:'getcheforders'}}).done(function(data){   
+            $("#allorder").html("");
+            for(i=0;i<data.vip.length;i++){
+            //	alert(data.vip[i].OrderID)
+            	
+            	
+   			server.request({route:{app:'chefr',id:data.vip[i].OrderID}}).done(function(data){ 
+   				
+   				console.log(data);
+   				
+		  	var source   = $("#orderlist").html();
+   			var template = Handlebars.compile(source);
+   			var html    = template( data );
+  			$("#allorder").append(html); 
+
+  			
+        	});
+            		 			
+
+  			}
+  			loadScript();
+            server.request({route:{app:'getcheforders'}}).done(function(data){   
+            console.log(data);
+           	var source   = $("#counts").html();
+   			var template = Handlebars.compile(source);
+   			var html    = template( data );
+  			$("#q").html(html); 
+           
+           
+  			});
+                        give();
+
+  			});}
+
+
+        	if(login=="true" ){
+if(isemployee==3){
+        		        	loadorders();
+        	
+          $("body").delegate("#done","click",function(){
+        	var id=$(this).data("id");
+        	var d=3;
+        	server.request({route:{app:'updateOrderChef',id:id,d:d}}).done(function(data){ 
+      	 	      	 	 notificationcorrect("DONE :) ");
+
+				loadorders();
+        	});
+			
+        	});
+        	$("body").delegate(".logout-div","click", function(){
+	localStorage.setItem("login",false);
+	localStorage.removeItem("loginID");
+
+	localStorage.removeItem("isemployee");
+	window.location="./index.html";
+});
+
+        	        	
+
+        	}else{
+$("._5body").html("");
+
+
+}
+
+}else{
+	window.location="./index.html";
+
+
+}
+function give(){
+
+
+
+
+
+  setTimeout(function(){
+
+
+  loadorders()
+}, 600000);  
+
+
+
+
+}
+ 
+  
+
+      	
+        	
+        	
+            
+////////////////////////////////////////////////////////////////////
+
+
+})();
+
+
---
+++ js/maping.js
@@ -0,0 +1,97 @@
+       var latitude=0;
+       var longitude=0;
+       	
+    var dslan =0;
+	var dslon =0;
+	
+	
+	function setlan(lan){
+		
+		dslan =lan;
+		
+	}
+	
+	function setlog (logn) {
+		 dslon =logn;
+	  
+	}
+	
+	function vip(){
+	
+  if (navigator.geolocation) {
+
+    navigator.geolocation.getCurrentPosition(function(position){
+
+    var map;
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+    var accuracy = position.coords.accuracy;
+    var coords = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+    var mapOptions = {
+        zoom: 15,
+        center: coords,
+        mapTypeControl: true,
+        navigationControlOptions: {
+            style: google.maps.NavigationControlStyle.SMALL
+        },
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+
+        map = new google.maps.Map(
+            document.getElementById("map"), mapOptions
+            );
+        var marker = new google.maps.Marker({
+                position: coords,
+                map: map,
+                title: "ok"
+        });
+        
+        
+        
+        var directionsDisplay = new google.maps.DirectionsRenderer({
+   			 map: map
+  		});
+
+ 		 // Set destination, origin and travel mode.
+ 		 var from = {lat:position.coords.latitude, lng:longitude = position.coords.longitude};
+ 		 var to = {lat: dslan, lng: dslon};
+ 		 var request = {
+  		  destination: to,
+   		  origin: from,
+   		  travelMode: google.maps.TravelMode.DRIVING
+  		};
+
+  // Pass the directions request to the directions service.
+  var directionsService = new google.maps.DirectionsService();
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      // Display the route on the map.
+      directionsDisplay.setDirections(response);
+    }
+  });
+        
+
+
+ 
+function calcRoute() {
+  var request = {
+    origin:start,
+    destination:end,
+    travelMode: google.maps.TravelMode.DRIVING
+  };
+  directionsService.route(request, function(result, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(result);
+    }
+  });
+}
+    },function error(msg){alert('Please enable your GPS position future.');  
+
+  }, {maximumAge:600000, timeout:5000, enableHighAccuracy: true});
+
+}else {
+    alert("Geolocation API is not supported in your browser.");
+}
+
+}
